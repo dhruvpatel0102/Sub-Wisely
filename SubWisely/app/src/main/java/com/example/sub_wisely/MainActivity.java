443235d135c960afaddef3dd7c76bf2309f87d56
dhruvpatel0102 <dhruvpatel9172@gmail.com>
2022-02-04 15:40:39 -0500

    First Commit

---
+++ SubWisely/app/src/main/java/com/example/sub_wisely/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sub_wisely;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
